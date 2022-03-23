8f7e225d9ce29c13c19d5974d9dc72bbad8f8a67
DAMJuncosa <71723517+DAMJuncosa@users.noreply.github.com>
2022-03-23 03:14:41 +0100

    OpenShift

--- Shabok/src/main/java/com/shabo/api/security/WebSecurity.java
+++ Shabok/src/main/java/com/shabo/api/security/WebSecurity.java
@@ -52,7 +52,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.cors().and()
 			.csrf().disable()
-			.authorizeRequests().antMatchers(HttpMethod.POST, LOGIN_URL, REGISTER_URL, "/libros").permitAll()
+			.authorizeRequests().antMatchers(HttpMethod.POST, LOGIN_URL, REGISTER_URL).permitAll()
+			.antMatchers(HttpMethod.GET, "/api/libros").permitAll()
 			.anyRequest().authenticated().and()
 				.addFilter(new JWTAuthenticationFilter(authenticationManager()))
 				.addFilter(new JWTAuthorizationFilter(authenticationManager()));
