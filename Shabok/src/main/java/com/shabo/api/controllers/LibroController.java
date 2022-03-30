863f8588e74b30403450ad269cf3b6bc8861b90b
DAMJuncosa <71723517+DAMJuncosa@users.noreply.github.com>
2022-03-31 01:00:31 +0200

    .

--- Shabok/src/main/java/com/shabo/api/controllers/LibroController.java
+++ Shabok/src/main/java/com/shabo/api/controllers/LibroController.java
@@ -5,6 +5,8 @@ package com.shabo.api.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -28,7 +30,7 @@ import com.shabo.api.services.UsuarioServiceImpl;
 @RestController
 @RequestMapping("/api")
 public class LibroController {
-	
+	Logger logger = LoggerFactory.getLogger("Pruebas");
 	@Autowired
 	private LibroServiceImpl librosServiceImpl;
 	
@@ -43,8 +45,10 @@ public class LibroController {
 	}
 	
 	@PutMapping("/libros/crear")
-	public Libro crearLibro(@RequestBody Libro libro) {
-		return librosServiceImpl.crearLibro(libro);
+	public Libro crearLibro(@RequestBody minilibro libro) {
+		logger.info(String.valueOf(libro.getISBN()));
+		Libro crearLibro = new Libro(libro.getISBN(), libro.getNombre(), libro.getDescripcion(), libro.getAutor());
+		return librosServiceImpl.crearLibro(crearLibro);
 	}
 	
 	@PostMapping("/libros/modificar/{id}")
@@ -65,5 +69,49 @@ public class LibroController {
 		librosServiceImpl.eliminarLibro(id);
 		
 	}
+	
+}
+
+class minilibro{
+	long ISBN;
+	String nombre;
+	String autor;
+	String descripcion;
+	
+	public minilibro() {
+		super();
+	}
+	public minilibro(long ISBN, String nombre, String autor, String descripcion) {
+		super();
+		this.ISBN = ISBN;
+		this.nombre = nombre;
+		this.autor = autor;
+		this.descripcion = descripcion;
+	}
+	public long getISBN() {
+		return ISBN;
+	}
+	public void setISBN(long iSBN) {
+		ISBN = iSBN;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
 	
 }
